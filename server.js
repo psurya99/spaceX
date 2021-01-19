b884a8204e26ecf87e54fed80ef1c637a7331797
psurya99 <suryaprakashpadala@gmail.com>
2021-01-19 11:56:15 +0530

    build changes

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const app=express();
 
 app.use (express.static(__dirname +'/dist/XT-App'));
 res.sendFile(path.join(__dirname +'/dist/XT-App/index.html'));
-// app.get('/*',function (req,res){
-//     res.sendFile(path.join(_dirname +'/dist/XT-App/index.html'));
-// });
-
 
 //start the app
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`))
